8b1c1a24262f36c86e2b70e216f3fd7a8cc2b8ce
Vinicios Tobias Da Silva Paixao <vinicios.paixao@meta.com.br>
2024-10-27 21:27:56 -0300

    adicionado base SpringBoot

---
+++ CSF-Solutions/src/main/java/CSF_Solutions/CSF_Solutions/CsfSolutionsApplication.java
@@ -0,0 +1,13 @@
+package CSF_Solutions.CSF_Solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsfSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsfSolutionsApplication.class, args);
+	}
+
+}
---
+++ CSF-Solutions/src/test/java/CSF_Solutions/CSF_Solutions/CsfSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package CSF_Solutions.CSF_Solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsfSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
